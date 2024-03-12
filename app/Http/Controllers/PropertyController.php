1bfcb9c9e072a3e83d2e285e48dd552e3f21ddc5
Fabian Bertrand <fabianbertrand94@gmail.com>
2024-03-12 10:17:55 -0400

    Created routes

---
+++ app/Http/Controllers/PropertyController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PropertyController extends Controller
+{
+     // Method for displaying the form to add a new property
+     public function createForm()
+     {
+         return view('properties.create');
+     }
+ 
+     // Method for displaying a list of all properties
+     public function index()
+     {
+         $properties = Property::all();
+         return view('properties.index', compact('properties'));
+     }
+ 
+     // Method for viewing an individual property by the specific property id
+     public function show($property_id)
+     {
+         $property = Property::findOrFail($property_id);
+         return view('properties.show', compact('property'));
+     }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,13 @@ Route::get('/about', function () {
     return view('about');
 });
 
-// Create additional Routes below
+// Create additional Routes below'
+
+// Route for displaying the form to add a new property
+Route::get('/properties/create', [PropertyController::class, 'createForm'])->name('properties.create');
+
+// Route for displaying a list of all properties
+Route::get('/properties', [PropertyController::class, 'index'])->name('properties.index');
+
+// Route for viewing an individual property by the specific property id
+Route::get('/properties/{property_id}', [PropertyController::class, 'show'])->name('properties.show');
