eea457f2e465f2a989c968b9eae009729ef22727
sorraine <kalifasroberts@gmail.com>
2024-03-12 11:09:53 -0400

    Changes

--- database/migrations/2024_03_12_144153_create-property-table.php
+++ database/migrations/2024_03_12_144153_create-property-table.php
@@ -11,7 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // here 
+        Schema::create('properties', function (Blueprint $table) {
+
+
+        });
     }
 
     /**
